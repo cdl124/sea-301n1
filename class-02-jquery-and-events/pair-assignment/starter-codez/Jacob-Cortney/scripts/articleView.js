df7cc3181d3a0068d530fe727d7184a5e6d40712
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-01-15 20:02:35 -0800

    fixed read on

--- class-02-jquery-and-events/pair-assignment/starter-codez/Jacob-Cortney/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/starter-codez/Jacob-Cortney/scripts/articleView.js
@@ -65,7 +65,7 @@ articleView.setTeasers = function() {
   $('.read-on').on('click', function (e) {
     e.preventDefault();
     $(this).hide();
-    $(this).parent().find('.article-body *:nth-of-type(n+2)').show();
+    $(this).parent().find('.article-body *:nth-of-type(n+2)').show();// no read shows only the article associated with button
   })
 };
 
