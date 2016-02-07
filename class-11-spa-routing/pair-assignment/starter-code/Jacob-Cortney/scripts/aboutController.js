9c22c118c30bc0dedb117787fcc4879e337d6150
Cortney Lane <cdlane124@gmail.com>
2016-02-06 16:08:10 -0800

    more tinkering around but still need to get it to work!

--- class-11-spa-routing/pair-assignment/starter-code/Jacob-Cortney/scripts/aboutController.js
+++ class-11-spa-routing/pair-assignment/starter-code/Jacob-Cortney/scripts/aboutController.js
@@ -3,7 +3,7 @@
 
   // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.index = function() {
-    $('#articles').hide().show('#about');
+    $('main').hide().show('#about');
   };
 
   module.aboutController = aboutController;
--- class-11-spa-routing/pair-assignment/starter-code/Jacob-Cortney/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/Jacob-Cortney/scripts/articleController.js
@@ -2,16 +2,15 @@
   var articlesController = {};
 
   // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
-  $(document).ready(function() {
-    Article.createTable();
-  })
+
+  Article.createTable();
 
   // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
     Article.fetchAll(articleView.initIndexPage);
-    $('main').hide().fadeIn('#articles', 300);
+    $('#articles').show().siblings().hide();
   };
 
   module.articlesController = articlesController;
