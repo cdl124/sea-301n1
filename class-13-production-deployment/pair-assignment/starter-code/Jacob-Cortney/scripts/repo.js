33130515d8d60c374f24a9abe67fbce1185d19d7
Cortney Lane <cdlane124@gmail.com>
2016-02-17 19:54:21 -0800

    add good folder

---
+++ class-13-production-deployment/pair-assignment/starter-code/Jacob-Cortney/scripts/repo.js
@@ -0,0 +1,25 @@
+(function(module) {
+  var repos = {};
+
+  repos.all = [];
+
+  // TODO: Refactor this ajax call into a get request to the proxy end point provided by server.js.
+  repos.requestRepos = function(callback) {
+    $.get('/github/users/cdl124/repos' +
+            '?per_page=100' +
+            '&sort=updated')
+      .done(function(data, message, xhr) {
+        repos.all = data;
+        console.log(repos.all)
+      })
+    .done(callback);
+  };
+
+  repos.with = function(attr) {
+    return repos.all.filter(function(repo) {
+      return repo[attr];
+    });
+  };
+
+  module.repos = repos;
+})(window);
